a6e3599fb8d296a4b68a2b87b523a11e9a9f607f
NgoMinhhh <ngonhatminh9898@gmail.com>
2024-09-20 22:21:05 +0930

    Startup

---
+++ NavisPlugin/Class1.vb
@@ -0,0 +1,45 @@
+﻿Imports Autodesk.Navisworks.Api
+Imports Autodesk.Navisworks.Api.Plugins
+Imports Autodesk.Navisworks.Api.Application
+Imports Autodesk.Navisworks.Api.DocumentParts
+Imports System.Text
+
+Namespace BasicPlugIn
+    <Plugin("NavisPlugin",                         ' Plugin name
+            "ADSK",                                             ' 4 character Developer ID or GUID
+            ToolTip:="BasicPlugIn.ABasicPlugin tool tip",       ' The tooltip for the item in the ribbon
+            DisplayName:="Extract Properties Plugin")>                 ' Display name for the Plugin in the Ribbon
+    Public Class ABasicPlugin
+        Inherits AddInPlugin                                    ' Derives from AddInPlugin
+        ' Shared method equivalent to static in C#
+        Public Function IteratePropertyCategoryDataProperties()
+            ' Create a StringBuilder to store the category names
+            Dim output As New StringBuilder()
+            ' Iterate through the ModelItems in the current selection
+            For Each item As ModelItem In Application.ActiveDocument.CurrentSelection.SelectedItems
+                    ' Iterate through the item's PropertyCategory entries
+                    For Each category As PropertyCategory In item.PropertyCategories
+                        ' Iterate through the DataProperties
+                        For Each dataProperty As DataProperty In category.Properties
+                            ' Add content for display
+                            output.AppendFormat(
+                                "PropertyCategory: ({0} / {1})   DataProperty: CombinedName='{2}', Value='{3}'{4}",
+                                category.DisplayName,
+                                category.Name,
+                                dataProperty.CombinedName,
+                                If(dataProperty.Value Is Nothing, "", dataProperty.Value.ToString()),
+                                Environment.NewLine) ' Adds a new line after each entry
+                        Next
+                    Next
+                Next
+
+            Return output
+        End Function
+
+
+        Public Overrides Function Execute(ParamArray parameters() As String) As Integer
+            Debug.Print(IteratePropertyCategoryDataProperties)
+            Return 0
+        End Function
+    End Class
+End Namespace
---
+++ NavisPlugin/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("NavisPlugin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NavisPlugin")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5a84d651-0c73-4588-a1ca-584608d4d9e4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
