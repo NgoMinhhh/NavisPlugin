c29836e3a690951cf86719a8b10864202ca4f578
NgoMinhhh <ngonhatminh9898@gmail.com>
2024-09-21 00:39:46 +0930

    Fix extract function and finish write to file execute plugin

--- NavisPlugin/Class1.vb
+++ NavisPlugin/Class1.vb
@@ -3,6 +3,7 @@ Imports Autodesk.Navisworks.Api.Plugins
 Imports Autodesk.Navisworks.Api.Application
 Imports Autodesk.Navisworks.Api.DocumentParts
 Imports System.Text
+Imports System.IO
 
 Namespace BasicPlugIn
     <Plugin("NavisPlugin",                         ' Plugin name
@@ -12,33 +13,48 @@ Namespace BasicPlugIn
     Public Class ABasicPlugin
         Inherits AddInPlugin                                    ' Derives from AddInPlugin
         ' Shared method equivalent to static in C#
-        Public Function IteratePropertyCategoryDataProperties()
+
+        Public Function ExtractDataProperties() As List(Of String)
             ' Create a StringBuilder to store the category names
-            Dim output As New StringBuilder()
+            Dim output As New List(Of String)()
+
+            ' Write Header name
+            output.Add("ElementName,CategoryDisplayName,CategoryName,PropertyDisplayName,PropertyName,Value")
+
             ' Iterate through the ModelItems in the current selection
             For Each item As ModelItem In Application.ActiveDocument.CurrentSelection.SelectedItems
-                    ' Iterate through the item's PropertyCategory entries
-                    For Each category As PropertyCategory In item.PropertyCategories
-                        ' Iterate through the DataProperties
-                        For Each dataProperty As DataProperty In category.Properties
-                            ' Add content for display
-                            output.AppendFormat(
-                                "PropertyCategory: ({0} / {1})   DataProperty: CombinedName='{2}', Value='{3}'{4}",
-                                category.DisplayName,
-                                category.Name,
-                                dataProperty.CombinedName,
-                                If(dataProperty.Value Is Nothing, "", dataProperty.Value.ToString()),
-                                Environment.NewLine) ' Adds a new line after each entry
-                        Next
+                ' Iterate through the item's PropertyCategory entries
+                For Each category As PropertyCategory In item.PropertyCategories
+                    ' Iterate through the DataProperties
+                    For Each dataProperty As DataProperty In category.Properties
+                        ' Add content for display
+                        Dim line As String = String.Format("""{0}"",""{1}"",""{2}"",""{3}"",""{4}"",""{5}""",' Escape quotes
+                                                           item.DisplayName, 'Element's name
+                                                           category.DisplayName.Replace("""", """"""),
+                                                           category.Name.Replace("""", """"""),
+                                                           dataProperty.DisplayName.Replace("""", """"""),
+                                                           dataProperty.Name.Replace("""", """"""),
+                                                           If(dataProperty.Value Is Nothing, "", dataProperty.Value.ToString().Replace("""", """""")))
+                        output.Add(line)
                     Next
                 Next
+            Next
 
             Return output
         End Function
 
 
+
         Public Overrides Function Execute(ParamArray parameters() As String) As Integer
-            Debug.Print(IteratePropertyCategoryDataProperties)
+            'Extract properties from selected elements
+            Dim lines As List(Of String) = ExtractDataProperties()
+
+            ' Define the output CSV file path
+            Dim csvFilePath As String = "C:\Users\ngonh\Projects\NavisPlugin\sample.csv"
+
+            ' Write all lines to the CSV file at once
+            File.WriteAllLines(csvFilePath, lines, Encoding.UTF8)
+
             Return 0
         End Function
     End Class
