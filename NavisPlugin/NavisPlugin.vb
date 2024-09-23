e56e5356f242165983d623427d8a0ed05915cbce
NgoMinhhh <ngonhatminh9898@gmail.com>
2024-09-23 15:14:12 +0930

    Setup a basic docking pane

--- NavisPlugin/NavisPlugin.vb
+++ NavisPlugin/NavisPlugin.vb
@@ -348,3 +348,54 @@ Namespace NavisPlugin
     End Class
 #End Region
 End Namespace
+
+Namespace UnisaDockPaneAddin
+    <Plugin("UnisaDockPanePlugin", "CAPS", DisplayName:="UnisaDockPanPlugin", ToolTip:="UniSA Docking Pane Plugin")>
+    <DockPanePlugin(200, 400)>
+    Public Class UnisaDockPanePlugin
+        Inherits DockPanePlugin
+
+        Public Overrides Function CreateControlPane() As Control
+            Dim helloWorldControl As New Control With {
+                .Dock = DockStyle.Fill,
+                .Text = Me.TryGetString("HelloWorldText")
+            }
+            helloWorldControl.CreateControl()
+            Return helloWorldControl
+        End Function
+
+        Public Overrides Sub DestroyControlPane(pane As Control)
+            pane.Dispose()
+        End Sub
+    End Class
+
+    <Plugin("UnisaDockPaneAddin", "CAPS",
+        DisplayName:="BasicDockPaneAddin",
+        ToolTip:="Basic Dock Pane Plugin")>
+    Public Class BasicDockPaneAddin
+        Inherits AddInPlugin
+        Public Overrides Function Execute(ParamArray parameters() As String) As Integer
+            If Autodesk.Navisworks.Api.Application.IsAutomated Then
+                Throw New InvalidOperationException("Invalid when running using Automation")
+            End If
+
+            'Find the plugin
+            Dim pr As PluginRecord = Autodesk.Navisworks.Api.Application.Plugins.FindPlugin("UnisaDockPanePlugin.CAPS")
+
+            If pr IsNot Nothing AndAlso TypeOf pr Is DockPanePluginRecord AndAlso pr.IsEnabled Then
+                'check if it needs loading
+                If pr.LoadedPlugin Is Nothing Then
+                    pr.LoadPlugin()
+                End If
+
+                Dim dpp As DockPanePlugin = TryCast(pr.LoadedPlugin, DockPanePlugin)
+                If dpp IsNot Nothing Then
+                    'switch the Visible flag
+                    dpp.Visible = Not dpp.Visible
+                End If
+            End If
+
+            Return 0
+        End Function
+    End Class
+End Namespace
