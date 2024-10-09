0227501d7e6536c21f2cc6fd223b76e54f9264c9
NgoMinhhh <ngonhatminh9898@gmail.com>
2024-10-09 23:27:32 +1030

    add a demo to create a folder with child selection sets

--- NavisPlugin/NavisPlugin.vb
+++ NavisPlugin/NavisPlugin.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Windows.Forms
+Imports Autodesk.Navisworks.Api
 Imports Autodesk.Navisworks.Api.Plugins
+Imports Application = Autodesk.Navisworks.Api.Application
 
 Namespace UnisaDockPaneAddin
 	<Plugin("UnisaDockPanePlugin", "MINH", DisplayName:="Unisa Plugin", ToolTip:="UniSA Docking Pane Plugin For LoD Verification")>
@@ -46,6 +48,40 @@ Namespace UnisaDockPaneAddin
 				'        dpp.Visible = Not dpp.Visible
 				'    End If
 			End If
+
+			Return 0
+		End Function
+	End Class
+
+	<Plugin("TestPlugin", "MINH",
+		DisplayName:="TestPlugin")>
+	Public Class TestPlugin
+		Inherits AddInPlugin
+		Public Overrides Function Execute(ParamArray parameters() As String) As Integer
+			If Autodesk.Navisworks.Api.Application.IsAutomated Then
+				Throw New InvalidOperationException("Invalid when running using Automation")
+			End If
+			Dim activeDoc As Document = Application.ActiveDocument
+			Try
+				Dim selectionSet1 As New SelectionSet() With {
+					.DisplayName = "TestChildren1"}
+				Dim selectionSet2 As New SelectionSet() With {
+					.DisplayName = "TestChildren2",
+					.Guid = Guid.NewGuid()}
+				Dim folderItem As New FolderItem() With {
+					.DisplayName = "Test"
+				}
+				activeDoc.SelectionSets.AddCopy(folderItem)
+
+				For Each item In activeDoc.SelectionSets.Value.ToList()
+					If item.DisplayName = "Test" Then
+						activeDoc.SelectionSets.AddCopy(item, selectionSet2)
+						activeDoc.SelectionSets.AddCopy(item, selectionSet1)
+					End If
+				Next
+			Catch ex As Exception
+				Debug.Print(ex.Message)
+			End Try
 
 			Return 0
 		End Function
